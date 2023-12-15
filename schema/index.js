4ba5e29e02fc917e758a411be53c8629edefcaf8
michelp76 <michel.puissant@inextenso.fr>
2023-12-15 17:06:20 +0100

    Almost end of Part 6

--- schema/index.js
+++ schema/index.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const graphql_import_1 = require("graphql-import");
-const graphql_tools_1 = require("graphql-tools");
+const schema_1 = require("@graphql-tools/schema");
 const resolvers_1 = __importDefault(require("./resolvers"));
 const typeDefs = (0, graphql_import_1.importSchema)('schema/typeDefs.graphql');
-exports.default = (0, graphql_tools_1.makeExecutableSchema)({ resolvers: resolvers_1.default, typeDefs });
+exports.default = (0, schema_1.makeExecutableSchema)({ resolvers: resolvers_1.default, typeDefs });
--- schema/resolvers.js
+++ schema/resolvers.js
@@ -6,13 +6,20 @@ const resolvers = {
     Date: graphql_scalars_1.DateTimeResolver,
     URL: graphql_scalars_1.URLResolver,
     Chat: {
+        messages(chat) {
+            return db_1.messages.filter((m) => chat.messages.includes(m.id));
+        },
         lastMessage(chat) {
-            return db_1.messages.find((m) => m.id === chat.lastMessage);
+            const lastMessage = chat.messages[chat.messages.length - 1];
+            return db_1.messages.find((m) => m.id === lastMessage);
         },
     },
     Query: {
         chats() {
             return db_1.chats;
+        },
+        chat(root, { chatId }) {
+            return db_1.chats.find((c) => c.id === chatId);
         },
     },
 };
